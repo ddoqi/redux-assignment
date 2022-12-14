ec4879489eb7bb5ac97e29adad229c2e2a459131
hongdakyung <bongbk72@naver.com>
2022-12-14 13:01:20 +0900

    숙련과제 커밋

---
+++ src/redux/config/configStore.js
@@ -0,0 +1,10 @@
+import { createStore, combineReducers } from "redux";
+import todosReducer from "../modules/todosReducer.js";
+
+const rootReducer = combineReducers({
+  todosReducer,
+});
+
+const store = createStore(rootReducer);
+
+export default store;
